e10ac4191a143dcf2a923135d230f46fe8a80793
sans-creator <sanskarj163@gmail.com>
2025-07-03 23:57:08 +0530

    index route

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const path = require('path');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send("server working fine");
-});
+
 
 
 app.set('view engine', 'ejs');
@@ -14,6 +12,27 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 port=8080
+
+let posts=[
+    {username:"sans",
+    content:"i love coding",
+},
+{
+    username:"john",
+    content:"i love javascript",
+}, 
+{
+    username:"doe",
+    content:"i love python",
+
+}]
+
+
+app.get('/posts', (req, res) => {
+    res.render("index.ejs",{posts});
+});
+
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 }   );
